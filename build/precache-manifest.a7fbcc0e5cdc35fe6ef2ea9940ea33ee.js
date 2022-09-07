77792df0c63a916cc9db863cf003864333bdcb7c
Guillaume <guillaume.kerbourch@yahoo.com>
2022-09-07 05:31:51 +0200

    updated files

---
+++ build/precache-manifest.a7fbcc0e5cdc35fe6ef2ea9940ea33ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d61bc7bb068a770cf6eca0295171a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c4dbba7246ffd5165e",
+    "url": "/static/css/main.15bf71aa.chunk.css"
+  },
+  {
+    "revision": "b180de129fe8ba289aa4",
+    "url": "/static/js/2.d2d7c0a1.chunk.js"
+  },
+  {
+    "revision": "a6c4dbba7246ffd5165e",
+    "url": "/static/js/main.e978e40e.chunk.js"
+  },
+  {
+    "revision": "3a2272f4634eea45a997",
+    "url": "/static/js/runtime-main.02887672.js"
+  },
+  {
+    "revision": "a55a5aa33744d9d0c759d774f4fea92a",
+    "url": "/static/media/Croix.a55a5aa3.svg"
+  },
+  {
+    "revision": "bea3cace01fd1bb7cce02538f2f1e9ad",
+    "url": "/static/media/IconeTwitch.bea3cace.svg"
+  },
+  {
+    "revision": "29c57c4d128e946c27b22138c5ddba5b",
+    "url": "/static/media/MenuIco.29c57c4d.svg"
+  },
+  {
+    "revision": "862e4fe3b1103d98eab47fc95a10f678",
+    "url": "/static/media/Search.862e4fe3.svg"
+  }
+]);
